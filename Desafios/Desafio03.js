108de886a8ddf486dd2cf06b714bc8631e3c16ab
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2024-12-11 19:31:12 -0300

    Adiciona o desafio03.js e algumas mudanças no app.js

---
+++ Desafios/Desafio03.js
@@ -0,0 +1,26 @@
+let numero1 = 1;
+
+while(numero1 < 11 ){
+    alert(numero1);
+    numero1++;
+}
+
+let numero2 = 10;
+
+while(numero2 >= 0 ){
+    alert(numero2);
+    numero2--;
+}
+
+let numero3 = prompt('Digite um número para fazer uma ordem decrescente');
+let zero = 0;
+while(zero <= numero3){
+    alert(zero);
+    zero++;
+}
+
+let numero4 = prompt('Digite um número para fazer uma ordem decrescente');
+while(numero4 >= 0 ){
+    alert(numero4);
+    numero4--;
+}
