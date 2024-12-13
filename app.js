8c3e9795322e7cd360c80d96c3825f8fde176701
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2024-12-13 10:54:56 -0300

    Add random number generation and update user prompt for guessing game

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+let numeroSecreto = gerarNumeroAleatorio();
 function exibirTextoNatela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 };
 
 exibirTextoNatela('h1','Jogo do número secreto')
-exibirTextoNatela('p', 'Escolha um número de 1 a 100')
+exibirTextoNatela('p', 'Escolha um número de 1 a 10')
 
 function verificarChute(){
-    
+    console.log(numeroSecreto)
 };
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10+1);
+}
