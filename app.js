79416a64fba84d1a7aa54e53c3da6a88f9a34d0e
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2024-12-13 12:07:33 -0300

    Refactor game logic to include a restart function, improve initial message display, and enhance user interaction with button feedback

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 function exibirTextoNatela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 };
 
-exibirTextoNatela('h1','Jogo do número secreto')
-exibirTextoNatela('p', 'Escolha um número de 1 a 10')
+function exibirMensagemInicial() {
+    exibirTextoNatela('h1','Jogo do número secreto')
+    exibirTextoNatela('p', 'Escolha um número de 1 a 10')
+}
+
+exibirMensagemInicial()
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto)
+    
+    if (chute == numeroSecreto){
+        exibirTextoNatela('h1', 'Acertou');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`
+        exibirTextoNatela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else{
+        if(chute > numeroSecreto){
+            exibirTextoNatela('p','O número secreto é menor');
+        } else{
+            exibirTextoNatela('p','O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
 };
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10+1);
+}
+
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo()
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
