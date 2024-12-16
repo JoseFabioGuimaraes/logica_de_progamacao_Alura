d104d32ecb1a33c247a229770adb46e25ff89f3a
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2024-12-16 11:49:35 -0300

    Add initial implementation for desafio4 with JavaScript functions, HTML structure, and CSS styling; include buttons for user interaction

---
+++ Desafio_listaEFuncoes/desafio4/app.js
@@ -0,0 +1,22 @@
+let listaGenerica = [];
+
+let linguagensDeProgramacao = ['Javascript','python', 'Java'];
+
+linguagensDeProgramacao.push('C++');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('C');
+
+linguagensDeProgramacao[0];
+linguagensDeProgramacao[1];
+console.log(linguagensDeProgramacao[linguagensDeProgramacao.length - 1]);
+
+
+/* 
+Desafios:
+    Crie uma lista vazia, com o nome listaGenerica.
+    Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+    Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+    Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+    Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+    Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+*/
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let NumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -40,7 +41,13 @@ function verificarChute(){
 };
 
 function gerarNumeroAleatorio() {
-    let numeroEscolhido =  parseInt(Math.random() * 10+1);
+    let numeroEscolhido =  parseInt(Math.random() * numeroLimite +1);
+    let quantElementosLista = NumerosSorteados.length;
+
+    if(quantElementosLista == numeroLimite){
+        NumerosSorteados = []
+    }
+
     if (NumerosSorteados.includes(numeroEscolhido)){
         return gerarNumeroAleatorio();
     } else {
